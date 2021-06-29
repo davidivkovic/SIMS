68ab0b4fed4c1e9eee29a4e9e9b89d33eafb4a3b
milicad00 <draca.milica10@gmail.com>
2021-06-29 23:35:34 +0200

    Update AccountUpdate.razor

--- src/TailwindBlazorElectron/Model/Subscription.cs
+++ src/TailwindBlazorElectron/Model/Subscription.cs
@@ -13,5 +13,12 @@ namespace TailwindBlazorElectron.Model
         public DateTime StartDate { get; set; }
         public DateTime EndTime { get; set; }
         public double Price{ get; set; }
-    }
+		public int RemainingTime() { return (EndTime - DateTime.Now).Days; } 
+
+		public double CurrentPercentage() 
+		{
+			int TotalDays = (EndTime - StartDate).Days;
+			return 100 * (TotalDays - RemainingTime()) / TotalDays;
+		}
+	}
 }
